7404bc0e741501810aa55734c48ae2422cb96e02
bibica <119997727+bibicadotnet@users.noreply.github.com>
2026-01-26 18:45:58 +0700

    Update doh3.go

--- pkg/server/doh3.go
+++ pkg/server/doh3.go
@@ -49,7 +49,7 @@ func (s *Server) ServeH3(l *quic.EarlyListener) error {
 	hs := &http3.Server{
 		Handler:        &sHandler{s.opts.HttpHandler},
 		IdleTimeout:    idleTimeout,
-		MaxHeaderBytes: 2048,
+		MaxHeaderBytes: 4096,
 	}
 	if ok := s.trackCloser(hs, true); !ok {
 		return ErrServerClosed
