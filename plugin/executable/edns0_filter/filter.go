63013d055c2026be3ba572f834295d9fc69f882d
bibica <119997727+bibicadotnet@users.noreply.github.com>
2026-02-10 17:10:23 +0700

    Update filter.go

--- plugin/executable/edns0_filter/filter.go
+++ plugin/executable/edns0_filter/filter.go
@@ -1,22 +1,3 @@
-/*
- * Copyright (C) 2020-2022, IrineSistiana
- *
- * This file is part of mosdns.
- *
- * mosdns is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * mosdns is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
-
 package edns0_filter
 
 import (
@@ -44,11 +25,9 @@ func init() {
 
 type Args struct {
 	// Args priority: NoEDNS > Keep > Discard.
-	// If both Keep and Discard is not specified. edns0_filter will
-	// keep no EDNS0 option (discard all EDNS0 options).
-	NoEDNS  bool     `yaml:"no_edns"` // Remove entire EDNS0 RR.
-	Keep    []uint16 `yaml:"accept"`  // Only keep those EDNS0 options and discard others.
-	Discard []uint16 `yaml:"discard"` // Only remove those EDNS0 options and keep others.
+	NoEDNS  bool     `yaml:"no_edns"`
+	Keep    []uint16 `yaml:"accept"`
+	Discard []uint16 `yaml:"discard"`
 }
 
 var _ coremain.ExecutablePlugin = (*Filter)(nil)
@@ -69,8 +48,9 @@ func NewFilter(bp *coremain.BP, args *Args) *Filter {
 		if len(opts) == 0 {
 			return nil
 		}
-		m := make(map[uint16]struct{})
-		for _, option := range opts { // FIXED: Use the correct variable `opts` instead of `args.Keep`.
+		// Pre-allocation to avoid dynamic rehashing during boot
+		m := make(map[uint16]struct{}, len(opts))
+		for _, option := range opts {
 			m[option] = struct{}{}
 		}
 		return m
@@ -86,7 +66,7 @@ func NewFilter(bp *coremain.BP, args *Args) *Filter {
 
 func (s *Filter) Exec(ctx context.Context, qCtx *query_context.Context, next executable_seq.ExecutableChainNode) error {
 	q := qCtx.Q()
-	s.applyFilter(q)
+	s.applyFilter(q) // FAIL-FAST: Trust the architecture. If q is nil, panic here.
 	return executable_seq.ExecChainNode(ctx, qCtx, next)
 }
 
@@ -94,35 +74,37 @@ func (s *Filter) applyFilter(q *dns.Msg) {
 	switch {
 	case s.args.NoEDNS:
 		dnsutils.RemoveEDNS0(q)
+
 	case len(s.keep) > 0:
 		opt := q.IsEdns0()
 		if opt == nil || len(opt.Option) == 0 {
 			break
 		}
+		// OPTIMAL: Reuse backing array without closure overhead
 		opts := opt.Option[:0]
-		for i := range opt.Option {
-			if _, accept := s.keep[opt.Option[i].Option()]; accept {
-				opts = append(opts, opt.Option[i])
+		for _, o := range opt.Option {
+			if _, accept := s.keep[o.Option()]; accept {
+				opts = append(opts, o)
 			}
 		}
 		opt.Option = opts
+
 	case len(s.discard) > 0:
 		opt := q.IsEdns0()
 		if opt == nil || len(opt.Option) == 0 {
 			break
 		}
 		opts := opt.Option[:0]
-		for i := range opt.Option {
-			if _, remove := s.discard[opt.Option[i].Option()]; !remove {
-				opts = append(opts, opt.Option[i])
+		for _, o := range opt.Option {
+			if _, remove := s.discard[o.Option()]; !remove {
+				opts = append(opts, o)
 			}
 		}
 		opt.Option = opts
+
 	default: // remove all edns0 options
-		opt := q.IsEdns0()
-		if opt == nil || len(opt.Option) == 0 {
-			break
+		if opt := q.IsEdns0(); opt != nil {
+			opt.Option = nil // ZERO-ALLOCATION
 		}
-		opt.Option = make([]dns.EDNS0, 0)
 	}
 }
