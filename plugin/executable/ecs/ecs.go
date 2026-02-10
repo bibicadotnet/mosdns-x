9d2260968fd8714f56d2dbc1e4805fdec57c7ecf
bibica <119997727+bibicadotnet@users.noreply.github.com>
2026-02-10 22:19:48 +0700

    Update ecs.go

--- plugin/executable/ecs/ecs.go
+++ plugin/executable/ecs/ecs.go
@@ -1,20 +1,5 @@
 /*
- * Copyright (C) 2020-2022, IrineSistiana
- *
- * This file is part of mosdns.
- *
- * mosdns is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * mosdns is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * Copyright (C) 2020-2026, IrineSistiana
  */
 
 package ecs
@@ -46,28 +31,20 @@ func init() {
 var _ coremain.ExecutablePlugin = (*ecsPlugin)(nil)
 
 type Args struct {
-	// Automatically append client address as ecs.
-	// If this is true, pre-set addresses will not be used.
-	Auto bool `yaml:"auto"`
-
-	// force overwrite existing ecs
-	ForceOverwrite bool `yaml:"force_overwrite"`
-
-	// mask for ecs
-	Mask4 int `yaml:"mask4"` // default 24
-	Mask6 int `yaml:"mask6"` // default 48
-
-	// pre-set address
-	IPv4 string `yaml:"ipv4"`
-	IPv6 string `yaml:"ipv6"`
+	Auto           bool   `yaml:"auto"`
+	ForceOverwrite bool   `yaml:"force_overwrite"`
+	Mask4          int    `yaml:"mask4"` // default 24
+	Mask6          int    `yaml:"mask6"` // default 48
+	IPv4           string `yaml:"ipv4"`
+	IPv6           string `yaml:"ipv6"`
 }
 
 func (a *Args) Init() error {
 	if ok := utils.CheckNumRange(a.Mask4, 0, 32); !ok {
-		return fmt.Errorf("invalid mask4 %d, should between 0~32", a.Mask4)
+		return fmt.Errorf("invalid mask4 %d, should be 0~32", a.Mask4)
 	}
 	if ok := utils.CheckNumRange(a.Mask6, 0, 128); !ok {
-		return fmt.Errorf("invalid mask6 %d, should between 0~128", a.Mask6)
+		return fmt.Errorf("invalid mask6 %d, should be 0~128", a.Mask6)
 	}
 	utils.SetDefaultNum(&a.Mask4, 24)
 	utils.SetDefaultNum(&a.Mask6, 48)
@@ -89,32 +66,31 @@ func newPlugin(bp *coremain.BP, args *Args) (p *ecsPlugin, err error) {
 		return nil, err
 	}
 
-	ep := new(ecsPlugin)
-	ep.BP = bp
-	ep.args = args
-
-	if len(args.IPv4) != 0 {
-		addr, err := netip.ParseAddr(args.IPv4)
-		if err != nil {
-			return nil, fmt.Errorf("invaild ipv4 address, %w", err)
-		}
-		if !addr.Is4() {
-			return nil, fmt.Errorf("%s is not a ipv4 address", args.IPv4)
-		} else {
-			ep.ipv4 = addr
-		}
+	ep := &ecsPlugin{
+		BP:   bp,
+		args: args,
 	}
 
-	if len(args.IPv6) != 0 {
+	if args.IPv4 != "" {
+		addr, err := netip.ParseAddr(args.IPv4)
+		if err != nil {
+			return nil, fmt.Errorf("invalid ipv4 address %s: %w", args.IPv4, err)
+		}
+		if !addr.Is4() {
+			return nil, fmt.Errorf("%s is not an ipv4 address", args.IPv4)
+		}
+		ep.ipv4 = addr
+	}
+
+	if args.IPv6 != "" {
 		addr, err := netip.ParseAddr(args.IPv6)
 		if err != nil {
-			return nil, fmt.Errorf("invaild ipv6 address, %w", err)
+			return nil, fmt.Errorf("invalid ipv6 address %s: %w", args.IPv6, err)
 		}
 		if !addr.Is6() {
-			return nil, fmt.Errorf("%s is not a ipv6 address", args.IPv6)
-		} else {
-			ep.ipv6 = addr
+			return nil, fmt.Errorf("%s is not an ipv6 address", args.IPv6)
 		}
+		ep.ipv6 = addr
 	}
 
 	return ep, nil
@@ -122,6 +98,7 @@ func newPlugin(bp *coremain.BP, args *Args) (p *ecsPlugin, err error) {
 
 func (e *ecsPlugin) Exec(ctx context.Context, qCtx *query_context.Context, next executable_seq.ExecutableChainNode) error {
 	upgraded, newECS := e.addECS(qCtx)
+	
 	err := executable_seq.ExecChainNode(ctx, qCtx, next)
 	if err != nil {
 		return err
@@ -130,10 +107,8 @@ func (e *ecsPlugin) Exec(ctx context.Context, qCtx *query_context.Context, next 
 	if r := qCtx.R(); r != nil {
 		if upgraded {
 			dnsutils.RemoveEDNS0(r)
-		} else {
-			if newECS {
-				dnsutils.RemoveMsgECS(r)
-			}
+		} else if newECS {
+			dnsutils.RemoveMsgECS(r)
 		}
 	}
 	return nil
@@ -141,52 +116,36 @@ func (e *ecsPlugin) Exec(ctx context.Context, qCtx *query_context.Context, next 
 
 func (e *ecsPlugin) addECS(qCtx *query_context.Context) (upgraded bool, newECS bool) {
 	q := qCtx.Q()
+	if q == nil {
+		return false, false
+	}
+
 	opt := q.IsEdns0()
-	hasECS := opt != nil && dnsutils.GetECS(opt) != nil
-	if hasECS && !e.args.ForceOverwrite {
+	if opt != nil && dnsutils.GetECS(opt) != nil && !e.args.ForceOverwrite {
 		return false, false
 	}
 
 	var ecs *dns.EDNS0_SUBNET
-	if e.args.Auto { // use client ip
+	if e.args.Auto {
 		clientAddr := qCtx.ReqMeta().GetClientAddr()
 		if !clientAddr.IsValid() {
 			return false, false
 		}
-
-		var prefix netip.Prefix
-		if clientAddr.Is4() || clientAddr.Is4In6() {
-			prefix = netip.PrefixFrom(clientAddr.Unmap(), e.args.Mask4)
-		} else {
-			prefix = netip.PrefixFrom(clientAddr, e.args.Mask6)
-		}
-
-		maskedIP := prefix.Masked().Addr()
-		isV6 := maskedIP.Is6() && !maskedIP.Is4In6()
-		ecs = dnsutils.NewEDNS0Subnet(maskedIP.AsSlice(), uint8(prefix.Bits()), isV6)
-
-	} else { // use preset ip
-		switch {
-		case checkQueryType(q, dns.TypeA):
+		ecs = e.genECS(clientAddr)
+	} else if len(q.Question) > 0 {
+		qtype := q.Question[0].Qtype
+		switch qtype {
+		case dns.TypeA:
 			if e.ipv4.IsValid() {
-				prefix := netip.PrefixFrom(e.ipv4, e.args.Mask4)
-				maskedIP := prefix.Masked().Addr()
-				ecs = dnsutils.NewEDNS0Subnet(maskedIP.AsSlice(), uint8(e.args.Mask4), false)
+				ecs = e.genECS(e.ipv4)
 			} else if e.ipv6.IsValid() {
-				prefix := netip.PrefixFrom(e.ipv6, e.args.Mask6)
-				maskedIP := prefix.Masked().Addr()
-				ecs = dnsutils.NewEDNS0Subnet(maskedIP.AsSlice(), uint8(e.args.Mask6), true)
+				ecs = e.genECS(e.ipv6)
 			}
-
-		case checkQueryType(q, dns.TypeAAAA):
+		case dns.TypeAAAA:
 			if e.ipv6.IsValid() {
-				prefix := netip.PrefixFrom(e.ipv6, e.args.Mask6)
-				maskedIP := prefix.Masked().Addr()
-				ecs = dnsutils.NewEDNS0Subnet(maskedIP.AsSlice(), uint8(e.args.Mask6), true)
+				ecs = e.genECS(e.ipv6)
 			} else if e.ipv4.IsValid() {
-				prefix := netip.PrefixFrom(e.ipv4, e.args.Mask4)
-				maskedIP := prefix.Masked().Addr()
-				ecs = dnsutils.NewEDNS0Subnet(maskedIP.AsSlice(), uint8(e.args.Mask4), false)
+				ecs = e.genECS(e.ipv4)
 			}
 		}
 	}
@@ -202,11 +161,20 @@ func (e *ecsPlugin) addECS(qCtx *query_context.Context) (upgraded bool, newECS b
 	return false, false
 }
 
-func checkQueryType(m *dns.Msg, typ uint16) bool {
-	if len(m.Question) > 0 && m.Question[0].Qtype == typ {
-		return true
+func (e *ecsPlugin) genECS(addr netip.Addr) *dns.EDNS0_SUBNET {
+	var mask int
+	if addr.Is4() || addr.Is4In6() {
+		mask = e.args.Mask4
+		addr = addr.Unmap()
+	} else {
+		mask = e.args.Mask6
 	}
-	return false
+
+	prefix := netip.PrefixFrom(addr, mask)
+	maskedAddr := prefix.Masked().Addr()
+	isV6 := maskedAddr.Is6() && !maskedAddr.Is4In6()
+	
+	return dnsutils.NewEDNS0Subnet(maskedAddr.AsSlice(), uint8(mask), isV6)
 }
 
 type noECS struct {
@@ -216,12 +184,14 @@ type noECS struct {
 var _ coremain.ExecutablePlugin = (*noECS)(nil)
 
 func (n *noECS) Exec(ctx context.Context, qCtx *query_context.Context, next executable_seq.ExecutableChainNode) error {
-	dnsutils.RemoveMsgECS(qCtx.Q())
-	if err := executable_seq.ExecChainNode(ctx, qCtx, next); err != nil {
-		return err
+	if q := qCtx.Q(); q != nil {
+		dnsutils.RemoveMsgECS(q)
 	}
-	if qCtx.R() != nil {
-		dnsutils.RemoveMsgECS(qCtx.R())
+
+	err := executable_seq.ExecChainNode(ctx, qCtx, next)
+	
+	if r := qCtx.R(); r != nil {
+		dnsutils.RemoveMsgECS(r)
 	}
-	return nil
+	return err
 }
