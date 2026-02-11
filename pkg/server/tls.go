442431c24ca428dbab8b5109e64eddd19991fdbc
bibica <119997727+bibicadotnet@users.noreply.github.com>
2026-02-11 08:52:16 +0700

    Update tls.go

--- pkg/server/tls.go
+++ pkg/server/tls.go
@@ -282,7 +282,8 @@ func (s *Server) CreateETLSListner(l net.Listener, nextProtos []string, allowedS
 		CipherSuites: []uint16{
 			eTLS.TLS_AES_128_GCM_SHA256,
 			eTLS.TLS_CHACHA20_POLY1305_SHA256,
-			eTLS.TLS_AES_256_GCM_SHA384,
+			eTLS.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+			eTLS.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256,
 		},
 
 		CurvePreferences: []eTLS.CurveID{
