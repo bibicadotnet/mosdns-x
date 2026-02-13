c85859dcfd8b88988c9d306573ea64ea0dd79daa
bibica <119997727+bibicadotnet@users.noreply.github.com>
2026-02-13 10:44:00 +0700

    Update release.py

--- release.py
+++ release.py
@@ -1,4 +1,4 @@
-# !/usr/bin/env python3
+#!/usr/bin/env python3
 import argparse
 import datetime
 import logging
@@ -16,39 +16,20 @@ RELEASE_DIR = './release'
 
 logger = logging.getLogger(__name__)
 
-# more info: https://golang.org/doc/install/source
-# [(env : value),(env : value)]
+# Target environments
 envs = [
     [['GOOS', 'darwin'], ['GOARCH', 'amd64']],
     [['GOOS', 'darwin'], ['GOARCH', 'arm64']],
-    # [['GOOS', 'linux'], ['GOARCH', '386']],
-    [['GOOS', 'linux'], ['GOARCH', 'amd64']],
+    [['GOOS', 'linux'], ['GOARCH', 'amd64']], # Forced to v3 by logic below
     [['GOOS', 'linux'], ['GOARCH', 'amd64'], ['GOAMD64', 'v3']],
-
-    [['GOOS', 'linux'], ['GOARCH', 'arm'], ['GOARM', '5']],
-    [['GOOS', 'linux'], ['GOARCH', 'arm'], ['GOARM', '6']],
-    [['GOOS', 'linux'], ['GOARCH', 'arm'], ['GOARM', '7']],
+    [['GOOS', 'linux'], ['GOARCH', 'amd64'], ['GOAMD64', 'v4']],
     [['GOOS', 'linux'], ['GOARCH', 'arm64']],
-
-    # [['GOOS', 'linux'], ['GOARCH', 'mips'], ['GOMIPS', 'hardfloat']],
-    # [['GOOS', 'linux'], ['GOARCH', 'mips'], ['GOMIPS', 'softfloat']],
-    # [['GOOS', 'linux'], ['GOARCH', 'mipsle'], ['GOMIPS', 'hardfloat']],
     [['GOOS', 'linux'], ['GOARCH', 'mipsle'], ['GOMIPS', 'softfloat']],
-
-    # [['GOOS', 'linux'], ['GOARCH', 'mips64'], ['GOMIPS64', 'hardfloat']],
-    # [['GOOS', 'linux'], ['GOARCH', 'mips64'], ['GOMIPS64', 'softfloat']],
     [['GOOS', 'linux'], ['GOARCH', 'mips64le'], ['GOMIPS64', 'hardfloat']],
-    # [['GOOS', 'linux'], ['GOARCH', 'mips64le'], ['GOMIPS64', 'softfloat']],
-
     [['GOOS', 'linux'], ['GOARCH', 'ppc64le']],
-
-    # [['GOOS', 'freebsd'], ['GOARCH', '386']],
     [['GOOS', 'freebsd'], ['GOARCH', 'amd64']],
-
-    # [['GOOS', 'windows'], ['GOARCH', '386']],
     [['GOOS', 'windows'], ['GOARCH', 'amd64']],
 ]
-
 
 def go_build():
     logger.info(f'building {PROJECT_NAME}')
@@ -59,7 +40,6 @@ def go_build():
 
     VERSION = f'4.6.0'
     BuildTime = f'{datetime.datetime.now().strftime("%y.%m.%d")}'
-    print(f"Using version: {VERSION}")
 
     try:
         subprocess.check_call('go run ../ config gen config.yaml', shell=True, env=os.environ)
@@ -68,32 +48,52 @@ def go_build():
         raise
 
     for env in envs:
-        os_env = os.environ.copy()  # new env
+        os_env = os.environ.copy()
+        
+        # Build-time optimization: Pure Go static binary
+        os_env['CGO_ENABLED'] = '0'
 
+        for pairs in env:
+            os_env[pairs[0]] = pairs[1]
+
+        # Force GOAMD64=v3 for all amd64 targets if not specified
+        if os_env.get('GOARCH') == 'amd64' and 'GOAMD64' not in os_env:
+            os_env['GOAMD64'] = 'v3'
+
+        # Generate output name
         s = PROJECT_NAME
         for pairs in env:
-            os_env[pairs[0]] = pairs[1]  # add env
             s = s + '-' + pairs[1]
         zip_filename = s + '.zip'
-
+        
         suffix = '.exe' if os_env['GOOS'] == 'windows' else ''
         bin_filename = PROJECT_NAME + suffix
 
+        # PIE is only meaningful on Linux
+        buildmode = "-buildmode=pie" if os_env['GOOS'] == 'linux' else ""
+
         logger.info(f'building {zip_filename}')
         try:
+            # FIX: Using ../default.pgo because we are inside RELEASE_DIR
+            # Optimized flag order for compiler efficiency
             subprocess.check_call(
-                f'go build -pgo=auto -ldflags "-s -w -buildid= -X github.com/pmkol/mosdns-x/constant.Version={VERSION} -X github.com/pmkol/mosdns-x/constant.BuildTime={BuildTime}" -trimpath -o {bin_filename} ../', shell=True,
+                f'go build {buildmode} '
+                f'-pgo=../default.pgo '
+                f'-trimpath '
+                f'-ldflags "-s -w -buildid= '
+                f'-X github.com/pmkol/mosdns-x/constant.Version={VERSION} '
+                f'-X github.com/pmkol/mosdns-x/constant.BuildTime={BuildTime}" '
+                f'-o {bin_filename} ../', 
+                shell=True,
                 env=os_env)
 
             if args.upx:
                 try:
-                    subprocess.check_call(f'upx -9 -q {bin_filename}', shell=True, stderr=subprocess.DEVNULL,
-                                          stdout=subprocess.DEVNULL)
-                except subprocess.CalledProcessError as e:
-                    logger.error(f'upx failed: {e.args}')
+                    subprocess.check_call(f'upx -9 -q {bin_filename}', shell=True, stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
+                except Exception as e:
+                    logger.error(f'upx failed: {e}')
 
-            with zipfile.ZipFile(zip_filename, mode='w', compression=zipfile.ZIP_DEFLATED,
-                                 compresslevel=5) as zf:
+            with zipfile.ZipFile(zip_filename, mode='w', compression=zipfile.ZIP_DEFLATED, compresslevel=5) as zf:
                 zf.write(bin_filename)
                 zf.write('../README.md', 'README.md')
                 zf.write('./config.yaml', 'config.yaml')
@@ -101,16 +101,10 @@ def go_build():
 
         except subprocess.CalledProcessError as e:
             logger.error(f'build {zip_filename} failed: {e.args}')
-        except Exception:
-            logger.exception('unknown err')
-
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
-
-    if len(RELEASE_DIR) != 0:
-        if not os.path.exists(RELEASE_DIR):
-            os.mkdir(RELEASE_DIR)
-        os.chdir(RELEASE_DIR)
-
+    if not os.path.exists(RELEASE_DIR):
+        os.mkdir(RELEASE_DIR)
+    os.chdir(RELEASE_DIR)
     go_build()
