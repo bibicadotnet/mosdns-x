c4853ebef7e31e7b9f6f1716ff4562e9b5b1285b
bibica <119997727+bibicadotnet@users.noreply.github.com>
2026-02-17 15:57:08 +0700

    Update server.go

--- coremain/server.go
+++ coremain/server.go
@@ -1,22 +1,3 @@
-/*
- * Copyright (C) 2020-2022, IrineSistiana
- *
- * This file is part of mosdns.
- *
- * mosdns is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * mosdns is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
-
 package coremain
 
 import (
@@ -57,11 +38,18 @@ func (m *Mosdns) startServers(cfg *ServerConfig) error {
 		queryTimeout = time.Duration(cfg.Timeout) * time.Second
 	}
 
+	// Link blocking options from ServerConfig to EntryHandlerOpts
 	dnsHandler, err := D.NewEntryHandler(D.EntryHandlerOpts{
 		Logger:             m.logger,
 		Entry:              entry,
 		QueryTimeout:       queryTimeout,
 		RecursionAvailable: true,
+
+		// New early blocking options mapped from config
+		BlockAAAA:  cfg.BlockAAAA,
+		BlockPTR:   cfg.BlockPTR,
+		BlockHTTPS: cfg.BlockHTTPS,
+		BlockNoDot: cfg.BlockNoDot,
 	})
 	if err != nil {
 		return fmt.Errorf("failed to init entry handler, %w", err)
