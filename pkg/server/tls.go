caf141cea25ebf5df18f1ed9ce02e398c5057c18
bibica <119997727+bibicadotnet@users.noreply.github.com>
2026-02-20 20:45:41 +0700

    Update tls.go

--- pkg/server/tls.go
+++ pkg/server/tls.go
@@ -247,11 +247,12 @@ func (s *Server) CreateQUICListner(conn net.PacketConn, nextProtos []string, all
 			return cert, nil
 		},
 	}, &quic.Config{
-		Allow0RTT:                      true,
-		InitialStreamReceiveWindow:     1252,
-		MaxStreamReceiveWindow:         4 * 1024,
-		InitialConnectionReceiveWindow: 8 * 1024,
-		MaxConnectionReceiveWindow:     16 * 1024,
+	    Allow0RTT:                      true,
+	    InitialStreamReceiveWindow:     16 * 1024,
+	    MaxStreamReceiveWindow:         512 * 1024,
+	    InitialConnectionReceiveWindow: 32 * 1024,
+	    MaxConnectionReceiveWindow:     1024 * 1024,
+	    MaxIncomingStreams:              1000,
 	})
 }
 
