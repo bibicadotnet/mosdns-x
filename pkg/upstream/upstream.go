9dcdbe2c47254340e3879edd8a52915578e876e9
bibica <119997727+bibicadotnet@users.noreply.github.com>
2026-02-25 23:02:08 +0700

    Update upstream.go

--- pkg/upstream/upstream.go
+++ pkg/upstream/upstream.go
@@ -268,7 +268,7 @@ func NewUpstream(addr string, opt *Opt) (Upstream, error) {
 				}
 				return tlsConn, nil
 			},
-			TLSHandshakeTimeout:   7 * time.Second,
+			TLSHandshakeTimeout:   3 * time.Second,
 			ResponseHeaderTimeout: 10 * time.Second,
 			ExpectContinueTimeout: time.Second,
 			IdleConnTimeout:       idleConnTimeout,
